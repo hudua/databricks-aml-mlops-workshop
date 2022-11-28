dce04e25f4e3ba898c4d3305309d59d7ed1757ef
hudua <40040433+hudua@users.noreply.github.com>
2022-11-27 19:32:32 -0500

    Create scripts.sh

---
+++ databricks-connect/scripts.sh
@@ -0,0 +1,10 @@
+conda create -n adb_py38 python=3.8.10
+conda activate adb_py38
+conda install pip
+pip install databricks-connect==10.4.12
+pip install azureml-core
+pip install azureml-mlflow
+pip install numpy
+pip install pandas
+pip install scikit-learn
+pip install matplotlib
