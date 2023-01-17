fd0feeedcec3420c38ad9552e01fe5c8248895b4
hudua <40040433+hudua@users.noreply.github.com>
2023-01-16 19:44:17 -0500

    Update mount.py

--- utils/mount.py
+++ utils/mount.py
@@ -13,4 +13,16 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
+secret = dbutils.secrets.get(scope = 'kv', key = 'dlmountsp')
+configs = {"fs.azure.account.auth.type": "OAuth",
+       "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+       "fs.azure.account.oauth2.client.id": "<app (client) id>",
+       "fs.azure.account.oauth2.client.secret": secret,
+       "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/<tenant-id>/oauth2/token",
+       "fs.azure.createRemoteFileSystemDuringInitialization": "true"}
+
+dbutils.fs.mount(
+source = "abfss://<container-name>@<storage-name>.dfs.core.windows.net",
+mount_point = "/mnt/rawdfs",
+extra_configs = configs)
 
